7a485fced42249c29ad652aaf9538e22e5b6d4fa
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-09-23 22:32:46 +0600

    add

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/app';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return app(req, res);
-};
